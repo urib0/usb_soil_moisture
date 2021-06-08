f3e231884e5e916d0eec6db0e7c132d5d2f7f1aa
uribo <ur1b0.dev@gmail.com>
2021-06-08 22:54:50 +0900

    とりあえず動くし、バージョンの出力も入れた

--- usb_soil_moisture.ino
+++ usb_soil_moisture.ino
@@ -1,7 +1,27 @@
+#define PIN_AD A0
+#define STATUS_OK 0
+
 void setup()
 {
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    delay(10);
+    pinMode(PIN_AD, INPUT);
 }
 
 void loop()
 {
+    // put your main code here, to run repeatedly:
+    static int result = 0;
+
+    result = analogRead(PIN_AD);
+    Serial.print("moisture=");
+    Serial.print(result);
+    Serial.print(",");
+    Serial.print("123");
+    Serial.print(";status=");
+    Serial.print(STATUS_OK);
+    Serial.print("\n");
+
+    delay(1000);
 }
